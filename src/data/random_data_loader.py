24cc7eb79ef96ce93905c87edfb63a228b51fd0e
Xiangwei Wang <wangxiangwei.cpp@gmail.com>
2020-03-28 12:00:28 +0800

    update random data: add random value for unvalid pixel

--- src/data/random_data_loader.py
+++ src/data/random_data_loader.py
@@ -25,29 +25,38 @@ class RandomDataset(Dataset):
 
     def __getitem__(self, idx):
         image_0 = torch.Tensor(np.random.random((3,self.camera_parameter[1],self.camera_parameter[0])))#image_0
+        image_c = np.random.random((3,self.camera_parameter[1],self.camera_parameter[0]))#image_0
         depth_0 = torch.Tensor(np.random.random((self.camera_parameter[1],self.camera_parameter[0])))#depth
         motion_se = torch.Tensor((2*np.random.random((6))-1)*self.motion_ax)#motion
-        image_1= self.warp(image_0,depth_0,motion_se)
+        image_1= self.warp(image_0,image_c,depth_0,motion_se)
         image_f_01 = torch.Tensor( np.concatenate((image_0,image_1),axis=0))
 
         sample = {'image_f_01':image_f_01,'motion_f_01':motion_se*self.motion_ax_normalize}
         return sample
     def show_item(self):
         image_0 = np.random.random((3,self.camera_parameter[1],self.camera_parameter[0]))#image_0
+        image_c = np.random.random((3,self.camera_parameter[1],self.camera_parameter[0]))#image_0
         depth_0 = 100*np.random.random((self.camera_parameter[1],self.camera_parameter[0]))#depth
         motion_se = (2*np.random.random((6))-1)*self.motion_ax#motion
-        image_1= self.warp_np(image_0,depth_0,motion_se)
+        image_1= self.warp_np(image_0,image_c,depth_0,motion_se)
         return image_0.transpose(1,2,0),image_1.transpose(1,2,0),depth_0,motion_se
 
     def warp_np(self,image,depth,motion_se):
+        image_1,valid= inverse_warp(torch.Tensor(image).unsqueeze(0),torch.Tensor(depth).unsqueeze(0),\
+                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0)) 
+        image_1 = image_1.squeeze()
+        valid   = valid.squeeze()
         image_1= inverse_warp(torch.Tensor(image).unsqueeze(0),torch.Tensor(depth).unsqueeze(0),\
-                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0))[0].squeeze()
+                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0))[1].squeeze()
         return np.array(image_1)
 
-    def warp(self,image,depth,motion_se):
-        return inverse_warp(image.unsqueeze(0),depth.unsqueeze(0),\
-                motion_se.unsqueeze(0),self.camera_intrinsic.unsqueeze(0))[0].squeeze()
-                
+    def warp(self,image,image_c,depth,motion_se):
+        image_1,valid= inverse_warp(torch.Tensor(image).unsqueeze(0),torch.Tensor(depth).unsqueeze(0),\
+                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0)) 
+        image_1 = image_1.squeeze()
+        valid   = valid.squeeze()
+        return image_1+torch.Tensor(image_c)*(1-valid.float())
+             
 
 class RandomDatasetAdv(Dataset):
     def __init__(self,data_length=1000,
@@ -79,15 +88,20 @@ class RandomDatasetAdv(Dataset):
     def show_item(self,idx):
         image_0 = np.random.random((3,self.camera_parameter[1],self.camera_parameter[0]))#image_0
         depth_0 = 100*np.random.random((self.camera_parameter[1],self.camera_parameter[0]))#depth
+        image_1_c = np.random.random((3,self.camera_parameter[1],self.camera_parameter[0]))#image_0
         motion_se = (2*np.random.random((6))-1)*self.motion_ax#motion
         if self.motion is not None:
             motion_se = self.motion[idx,:]*self.motion_ax
-        image_1= self.warp_np(image_0,depth_0,motion_se)
+        image_1= self.warp_np(image_0,image_1_c,depth_0,motion_se)
+        print(image_1.shape)
         return image_0.transpose(1,2,0),image_1.transpose(1,2,0),depth_0,motion_se
 
-    def warp_np(self,image,depth,motion_se):
-        image_1= inverse_warp(torch.Tensor(image).unsqueeze(0),torch.Tensor(depth).unsqueeze(0),\
-                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0))[0].squeeze()
+    def warp_np(self,image,image_c,depth,motion_se):
+        image_1,valid= inverse_warp(torch.Tensor(image).unsqueeze(0),torch.Tensor(depth).unsqueeze(0),\
+                torch.Tensor(motion_se).unsqueeze(0),self.camera_intrinsic.unsqueeze(0)) 
+        image_1 = image_1.squeeze()
+        valid   = valid.squeeze()
+        image_1 = image_1+torch.Tensor(image_c)*(1-valid.float())
         return np.array(image_1)
 
     def warp(self,image,depth,motion_se):
